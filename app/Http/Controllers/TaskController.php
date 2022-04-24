656a1c276a7fb94530c235436c54b103e304e6aa
LoaiYoub <luaiayoubi@windowslive.com>
2022-04-24 20:51:58 +0300

    Eloquent used

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,30 +3,41 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Task;
 
 class TaskController extends Controller
 {
     public function index(){
-        $tasks = DB::table('tasks')->orderBy('name')->get();
-
+        //$tasks = DB::table('tasks')->orderBy('name')->get();
+        $tasks = Task::orderBy('name', 'Asc')->get();
         return view('tasks',compact('tasks'));
     }
     public function store(Request $request){
-        DB::table('tasks')->insert([
-          //  'name' => $_REQUEST['name']
-            'name' => $request->name,
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
+        // DB::table('tasks')->insert([
+        //   //  'name' => $_REQUEST['name']
+        //     'name' => $request->name,
+        //     'created_at' => now(),
+        //     'updated_at' => now()
+        // ]);
+
+        $task = new Task();
+        $task->name = $request->name;
+        $task->save();
 
         return redirect()->back();
     }
     public function delete($id){
-        DB::table('tasks')->where('id','=',$id)->delete();
-        return redirect()->back();
+        // DB::table('tasks')->where('id','=',$id)->delete();
+        $task = Task::where('id',$id)->delete();
+         return redirect()->back();
+
     }
-    public function update($id){
-        DB::table('tasks')->where('id','=',$id)->update(['name' => $_REQUEST['newName']]);
+    public function update($id, Request $request){
+        // DB::table('tasks')->where('id','=',$id)->update(['name' => $_REQUEST['newName']]);
+        $task = Task::where('id',$id);
+        $task-> update(['name' => $request->newName]);
+
         return redirect()->back();
+
     }
 }
